eb0640d1af5ed03d2080773d99bb00d1bbf457be
Arthur <arakhiyarov@gmail.com>
2022-10-12 20:30:53 +0500

    added cartitem and updated logic

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,23 @@
+import { useContext } from 'react';
+
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHadler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: props.amount,
+            price: props.price,
+        });
+    };
+
     return (
         <li>
             <div>
@@ -12,7 +26,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id}/>
+                <MealItemForm onAddToCart={addToCartHadler} />
             </div>
         </li>
     );
