8ca07de8bae0008ccb94edb9e7060be9f3b3172e
Arthur <arakhiyarov@gmail.com>
2022-10-13 15:29:21 +0500

    final components and add/remove logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,4 @@
 import { useReducer } from 'react';
-import { act } from 'react-dom/test-utils';
 
 import CartContext from './cart-context';
 
@@ -16,7 +15,6 @@ const cartReducer = (state, action) => {
             (item) => item.id === action.item.id
         );
         const existingCartItem = state.items[existingCartItemIndex];
-        let updatedItem;
         let updatedItems;
         if (existingCartItem) {
             const updatedItem = {
@@ -27,6 +25,30 @@ const cartReducer = (state, action) => {
             updatedItems[existingCartItemIndex] = updatedItem;
         } else {
             updatedItems = state.items.concat(action.item);
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
+    }
+
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.item.findIndex(
+            (item) => item.id === action.id
+        );
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id);
+        } else {
+            const updatedItem = {
+                ...existingItem,
+                amount: existingItem.amount - 1,
+            };
+            const updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
         }
         return {
             items: updatedItems,
